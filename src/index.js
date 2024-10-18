74d6abaf91fcb037c7ab74bc677e607cf1e020e5
prachishivwantsingh2002@gmail.com <prachishivwantsingh2002@gmail.com>
2024-10-18 09:22:20 +0530

    All files

---
+++ src/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+
+
+
+
+// import express from "express";
+// const app=express()
+
+// function connectDB(){
+
+// }
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("Error: ",error)
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+
+//         })
+
+//     }catch(error){
+//         console.error(error);
+//         throw err
+//     }
+//     })()
